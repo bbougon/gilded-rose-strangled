7001a71df701372717363ba4fd23683ceeb4d6e5
Bertrand Bougon <bertrandbougon@gmail.com>
2021-06-21 19:42:45 +0200

    Start implementing the strangler pattern

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-class GildedRose(object):
+class GildedRoseLegacy(object):
 
     def __init__(self, items):
         self.items = items
@@ -42,6 +42,34 @@ class Item:
         self.sell_in = sell_in
         self.quality = quality
 
+    def update_quality(self):
+        pass
+
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
+
+class ConjuredItem(Item):
+
+    def __init__(self, sell_in, quality):
+        super().__init__("Conjured", sell_in, quality)
+
+    def update_quality(self):
+        self.sell_in -= 1
+        self.quality -= 2 if self.sell_in > 0 else 4
+        if self.quality < 0:
+            self.quality = 0
+
+
+class GildedRose(object):
+
+    def __init__(self, items) -> None:
+        super().__init__()
+        self.items = items
+        self.conjured_items = [item for item in items if isinstance(item, ConjuredItem)]
+        self.normal_items = [item for item in items if item.name != "Conjured"]
+
+    def update_quality(self):
+        for item in self.conjured_items:
+            item.update_quality()
+        GildedRoseLegacy(self.normal_items).update_quality()
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,17 +1,67 @@
-# -*- coding: utf-8 -*-
-import unittest
-
-from gilded_rose import Item, GildedRose
+from python.gilded_rose import ConjuredItem, GildedRose, Item
 
 
-class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+def test_conjured_item_is_decaying_twice_as_fast():
+    item = ConjuredItem(10, 50)
 
-        
-if __name__ == '__main__':
-    unittest.main()
+    GildedRose(items=[item]).update_quality()
 
+    assert item.sell_in == 9
+    assert item.quality == 48
+
+
+def test_conjured_item_is_decaying_twice_as_fast_when_sell_in_has_passed():
+    item = ConjuredItem(0, 40)
+
+    GildedRose(items=[item]).update_quality()
+
+    assert item.sell_in == -1
+    assert item.quality == 36
+
+
+def test_conjured_item_quality_cannot_be_negative():
+    item = ConjuredItem(0, 1)
+
+    GildedRose(items=[item]).update_quality()
+
+    assert item.sell_in == -1
+    assert item.quality == 0
+
+
+def test_normal_item():
+    item = Item("Normal", 10, 50)
+
+    GildedRose(items=[item]).update_quality()
+
+    assert item.sell_in == 9
+    assert item.quality == 49
+
+
+def test_conjured_and_normal_items():
+    conjured_item = ConjuredItem(10, 50)
+    normal_item = Item("Normal", 10, 50)
+
+    GildedRose(items=[conjured_item, normal_item]).update_quality()
+
+    assert conjured_item.sell_in == 9
+    assert conjured_item.quality == 48
+    assert normal_item.sell_in == 9
+    assert normal_item.quality == 49
+
+
+def test_multiple_conjured_and_normal_items():
+    first_conjured_item = ConjuredItem(10, 50)
+    second_conjured_item = ConjuredItem(8, 46)
+    first_normal_item = Item("Normal", 10, 50)
+    second_normal_item = Item("Normal", 9, 40)
+
+    GildedRose(items=[first_conjured_item, second_conjured_item, first_normal_item, second_normal_item]).update_quality()
+
+    assert first_conjured_item.sell_in == 9
+    assert first_conjured_item.quality == 48
+    assert second_conjured_item.sell_in == 7
+    assert second_conjured_item.quality == 44
+    assert first_normal_item.sell_in == 9
+    assert first_normal_item.quality == 49
+    assert second_normal_item.sell_in == 8
+    assert second_normal_item.quality == 39
